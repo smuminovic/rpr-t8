2e034e12058635c269d9a84f510db89629bf1acd
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-12-14 11:53:38 +0100

    main

--- src/ba/unsa/etf/rpr/tutorijal08/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal08/Main.java
@@ -6,13 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Pretraga datoteka");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+
         primaryStage.show();
     }
 
